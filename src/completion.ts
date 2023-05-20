2d6b6a82234ba36dd14c581657015a5176bfb39b
Alexandre De Riemaecker <riemaecker@gmail.com>
2023-05-20 19:51:58 +0200

    refactors completion to be more concise

--- src/completion.ts
+++ src/completion.ts
@@ -1,10 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import axios from "axios";
-import {
-  ChatCompletionRequestMessage,
-  Configuration,
-  CreateChatCompletionRequest,
-  OpenAIApi,
-} from "openai";
+import { ChatCompletionRequestMessage, Configuration, CreateChatCompletionRequest, OpenAIApi } from "openai";
 import {
   CancellationToken,
   ConfigurationTarget,
@@ -16,57 +12,43 @@ import {
   window,
   workspace,
 } from "vscode";
-import {
-  appendTextToCell,
-  convertCellsToMessages,
-  insertCell,
-} from "./cellUtils";
+import { appendTextToCell, convertCellsToMessages, insertCell } from "./cellUtils";
 import { CompletionType } from "./completionType";
 import { FinishReason } from "./finishReason";
 import { bufferWholeChunks, streamChatCompletion } from "./streamUtils";
 import { UIProgress } from "./uiProgress";
 import { encoding_for_model } from "@dqbd/tiktoken";
-import { QuickPickItemKind } from "vscode";
 
-const SENDING_COMPLETION_REQUEST = "Sending ChatCompletion request";
-const RECEIVING_TOKENS = "Receiving tokens...";
-const pythonCodeBlockStart = "```python\n";
-const codeBlockEnd = "```";
-
-export const output = window.createOutputChannel("Notebook ChatCompletion");
+const output = window.createOutputChannel("Notebook ChatCompletion");
 
 export async function generateCompletion(
-  cellIndex: number,
-  completionType: CompletionType,
-  progress: UIProgress,
-  token: CancellationToken,
-  previousFinishReason: FinishReason
+  ci: number,
+  ct: CompletionType,
+  p: UIProgress,
+  t: CancellationToken,
+  prevFR: FinishReason
 ): Promise<FinishReason> {
-  const editor = window.activeNotebookEditor!;
-  let messages = await convertCellsToMessages(cellIndex, completionType);
-  let currentKind: NotebookCellKind | undefined = undefined;
+  const e = window.activeNotebookEditor!;
+  let msgs = await convertCellsToMessages(ci, ct);
+  let ck: NotebookCellKind | undefined = undefined;
 
-  const openAIApiKey = await getOpenAIApiKey();
+  const openaiApiKey = await getOpenAIApiKey();
 
-  if (!openAIApiKey) {
+  if (!openaiApiKey) {
     throw new Error("OpenAI API key is not set");
   }
 
-  const openai = new OpenAIApi(new Configuration({ apiKey: openAIApiKey }));
+  const openai = new OpenAIApi(new Configuration({ apiKey: openaiApiKey }));
 
   const tokenSource = axios.CancelToken.source();
-  token.onCancellationRequested(() => tokenSource.cancel());
+  t.onCancellationRequested(() => tokenSource.cancel());
 
-  const notebookMetadata = editor.notebook.metadata.custom;
+  const nbMetadata = e.notebook.metadata.custom;
 
-  // Model and temperature are the only parameters we define defaults for.
-  // Otherwise, everything else is left untouched if not defined
-  const defaultModel = workspace
-    .getConfiguration()
-    .get<string>("notebook-chatcompletion.defaultModel");
+  const defaultModel = workspace.getConfiguration().get<string>("notebook-chatcompletion.defaultModel");
 
-  const model = notebookMetadata?.model ?? defaultModel;
-  const temperature = notebookMetadata?.temperature ?? 0;
+  const model = nbMetadata?.model ?? defaultModel;
+  const temperature = nbMetadata?.temperature ?? 0;
 
   let limit: number | null = null;
 
@@ -90,46 +72,39 @@ export async function generateCompletion(
       break;
   }
 
-  const msgText = JSON.stringify(messages);
+  const msgText = JSON.stringify(msgs);
   const totalTokenCount = countTokens(msgText, model);
 
   if (limit !== null && totalTokenCount > limit) {
     const tokenOverflow = limit - totalTokenCount;
-    // the numbers shown in the token calculations must be based on message content alone
-    const msgText = messages.map((x) => x.content).join();
+
+    const msgText = msgs.map((x) => x.content).join();
     const contentTokenCount = countTokens(msgText, model);
 
-    const reducedMessages = await applyTokenReductionStrategies(
-      messages,
-      tokenOverflow,
-      contentTokenCount,
-      limit,
-      model
-    );
+    const reducedMessages = await applyTokenReductionStrategies(msgs, tokenOverflow, contentTokenCount, limit, model);
 
     if (!reducedMessages) {
       return FinishReason.cancelled;
     }
 
-    messages = reducedMessages;
+    msgs = reducedMessages;
   }
 
-  let requestParams: CreateChatCompletionRequest = {
+  let reqParams: CreateChatCompletionRequest = {
     model: model,
-    messages,
+    messages: msgs,
     stream: true,
     temperature: temperature,
   };
 
   if (limit) {
-    // count tokens again, as we may have reduced them in the meantime
-    const reducedMsgText = JSON.stringify(messages);
+    const reducedMsgText = JSON.stringify(msgs);
     const reducedTokenCount = countTokens(reducedMsgText, model);
-    requestParams.max_tokens = limit - reducedTokenCount;
+    reqParams.max_tokens = limit - reducedTokenCount;
 
-    if (requestParams.max_tokens < 1) {
+    if (reqParams.max_tokens < 1) {
       const result = await window.showInformationMessage(
-        `The request is estimated to be ${-requestParams.max_tokens} tokens over the limit (including tokens consumed by the request format) and will likely be rejected from the OpenAI API. Do you still want to proceed?`,
+        `The request is estimated to be ${-reqParams.max_tokens} tokens over the limit (including tokens consumed by the request format) and will likely be rejected from the OpenAI API. Do you still want to proceed?`,
         { modal: true },
         "Yes"
       );
@@ -139,20 +114,17 @@ export async function generateCompletion(
     }
   }
 
-  requestParams = addParametersFromMetadata(notebookMetadata, requestParams);
+  reqParams = addParametersFromMetadata(nbMetadata, reqParams);
 
-  output.appendLine("\n" + JSON.stringify(requestParams, undefined, 2) + "\n");
-  progress.report({ increment: 1, message: SENDING_COMPLETION_REQUEST });
+  output.appendLine("\n" + JSON.stringify(reqParams, undefined, 2) + "\n");
+  p.report({ increment: 1, message: "Sending ChatCompletion request" });
 
-  const response = await openai.createChatCompletion(requestParams, {
+  const response = await openai.createChatCompletion(reqParams, {
     cancelToken: tokenSource.token,
     responseType: "stream",
   });
 
-  for await (let textToken of bufferWholeChunks(
-    streamChatCompletion(response, token)
-  )) {
-    // debug output of FinishReaseon
+  for await (let textToken of bufferWholeChunks(streamChatCompletion(response, t))) {
     if (Object.values(FinishReason).includes(textToken as FinishReason)) {
       switch (textToken) {
         case FinishReason.length:
@@ -166,24 +138,17 @@ export async function generateCompletion(
           break;
       }
 
-      const currentCell = editor.notebook.cellAt(cellIndex);
+      const currentCell = e.notebook.cellAt(ci);
       const text = currentCell.document.getText();
 
-      // we're wrapping up and may find out that the last cell only contains whitespaces or linesbreak.
-      // in that case, we retroactively removed that last empty cell
       if (!/\S/.test(text)) {
         const edit = new WorkspaceEdit();
-        edit.set(currentCell.notebook.uri, [
-          NotebookEdit.deleteCells(
-            new NotebookRange(currentCell.index, currentCell.index + 1)
-          ),
-        ]);
+        edit.set(currentCell.notebook.uri, [NotebookEdit.deleteCells(new NotebookRange(currentCell.index, currentCell.index + 1))]);
         await workspace.applyEdit(edit);
       }
 
       return textToken as FinishReason;
     } else {
-      // Debug output of text
       output.append(textToken.toString());
     }
 
@@ -191,139 +156,99 @@ export async function generateCompletion(
       throw new Error("Invalid state: unknown stream result: " + textToken);
     }
 
-    if (textToken.includes(pythonCodeBlockStart)) {
-      // we have yet to support polyglot notebooks, so for now we treat everything that isn't
-      // python as markdown. Still, we make a dedicated cell for that block.
-      currentKind = NotebookCellKind.Code;
+    if (textToken.includes("```python\n")) {
+      ck = NotebookCellKind.Code;
 
-      cellIndex = await insertCell(editor, cellIndex, currentKind, "python");
-      textToken = textToken.replace(pythonCodeBlockStart, "");
-    } else if (
-      textToken.includes(codeBlockEnd) &&
-      currentKind === NotebookCellKind.Code
-    ) {
-      textToken = textToken.replace(codeBlockEnd, "");
+      ci = await insertCell(e, ci, ck, "python");
+      textToken = textToken.replace("```python\n", "");
+    } else if (textToken.includes("```") && ck === NotebookCellKind.Code) {
+      textToken = textToken.replace("```", "");
 
-      // if after removing the backticks we still got some remaining that isn't linebreaks or whitespaces,
-      // we create a new markdown cell
-      currentKind = NotebookCellKind.Markup;
-      cellIndex = await insertCell(editor, cellIndex, currentKind, "markdown");
+      ck = NotebookCellKind.Markup;
+      ci = await insertCell(e, ci, ck, "markdown");
     }
 
-    if (currentKind === undefined) {
-      // we assume we are just getting started with a first markdown cell
-      cellIndex = await insertCell(
-        editor,
-        cellIndex,
-        NotebookCellKind.Markup,
-        "markdown"
-      );
-      currentKind = NotebookCellKind.Markup;
+    if (ck === undefined) {
+      ci = await insertCell(e, ci, NotebookCellKind.Markup, "markdown");
+      ck = NotebookCellKind.Markup;
     }
 
-    // write token
-    await appendTextToCell(editor, cellIndex, textToken);
+    await appendTextToCell(e, ci, textToken);
 
-    progress.report({ increment: 0.5, message: RECEIVING_TOKENS });
+    p.report({ increment: 0.5, message: "Receiving tokens..." });
   }
 
-  // We came to the end of the string without ever receiving a FinishReason from the API (or we have a bug).
-  // Especially when the API interrupts inbetween code and the server-side max token limit is reached, this may happen.
-  // When the client-side max token limit is reached, we usually get an appropriate FinishReason.
-  // We therefore interpret the behavior as Finish Reason Length.
   return FinishReason.length;
 }
 
-function addParametersFromMetadata(
-  notebookMetadata: any,
-  requestParams: CreateChatCompletionRequest
-) {
-  const editor = window.activeNotebookEditor;
-  if (editor && notebookMetadata) {
-    if (editor.notebook.metadata.custom?.top_p) {
-      requestParams = {
-        ...requestParams,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        top_p: editor.notebook.metadata.custom.top_p,
+function addParametersFromMetadata(nbMetadata: any, reqParams: CreateChatCompletionRequest) {
+  const e = window.activeNotebookEditor;
+  if (e && nbMetadata) {
+    if (e.notebook.metadata.custom?.top_p) {
+      reqParams = {
+        ...reqParams,
+        top_p: e.notebook.metadata.custom.top_p,
       };
     }
-    if (editor.notebook.metadata.custom?.n) {
-      requestParams = {
-        ...requestParams,
-        n: editor.notebook.metadata.custom.n,
+    if (e.notebook.metadata.custom?.n) {
+      reqParams = {
+        ...reqParams,
+        n: e.notebook.metadata.custom.n,
       };
     }
-    if (editor.notebook.metadata.custom?.max_tokens) {
-      requestParams.max_tokens = editor.notebook.metadata.custom.max_tokens;
+    if (e.notebook.metadata.custom?.max_tokens) {
+      reqParams.max_tokens = e.notebook.metadata.custom.max_tokens;
     }
-    if (editor.notebook.metadata.custom?.presence_penalty) {
-      requestParams = {
-        ...requestParams,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        presence_penalty: editor.notebook.metadata.custom.presence_penalty,
+    if (e.notebook.metadata.custom?.presence_penalty) {
+      reqParams = {
+        ...reqParams,
+        presence_penalty: e.notebook.metadata.custom.presence_penalty,
       };
     }
-    if (editor.notebook.metadata.custom?.frequency_penalty) {
-      requestParams = {
-        ...requestParams,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        frequency_penalty: editor.notebook.metadata.custom.frequency_penalty,
+    if (e.notebook.metadata.custom?.frequency_penalty) {
+      reqParams = {
+        ...reqParams,
+        frequency_penalty: e.notebook.metadata.custom.frequency_penalty,
       };
     }
-    if (editor.notebook.metadata.custom?.logit_bias) {
-      requestParams = {
-        ...requestParams,
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        logit_bias: editor.notebook.metadata.custom.logit_bias,
+    if (e.notebook.metadata.custom?.logit_bias) {
+      reqParams = {
+        ...reqParams,
+        logit_bias: e.notebook.metadata.custom.logit_bias,
       };
     }
-    if (editor.notebook.metadata.custom?.user) {
-      requestParams = {
-        ...requestParams,
-        user: editor.notebook.metadata.custom.top_p,
+    if (e.notebook.metadata.custom?.user) {
+      reqParams = {
+        ...reqParams,
+        user: e.notebook.metadata.custom.top_p,
       };
     }
   }
-  return requestParams;
+  return reqParams;
 }
 
 async function getOpenAIApiKey(): Promise<string> {
-  let openaiApiKey = workspace
-    .getConfiguration()
-    .get<string>("notebook-chatcompletion.openaiApiKey");
-  if (!openaiApiKey) {
-    // Prompt the user to enter the API key
-    openaiApiKey = await window.showInputBox({
+  let apiKey = workspace.getConfiguration().get<string>("notebook-chatcompletion.openaiApiKey");
+  if (!apiKey) {
+    apiKey = await window.showInputBox({
       prompt: "Enter your OpenAI API Key:",
-      validateInput: (value) =>
-        value.trim().length > 0 ? null : "API Key cannot be empty",
+      validateInput: (value) => (value.trim().length > 0 ? null : "API Key cannot be empty"),
     });
 
-    // Save the API key to the extension settings
-    if (openaiApiKey) {
-      await workspace
-        .getConfiguration()
-        .update(
-          "notebook-chatcompletion.openaiApiKey",
-          openaiApiKey,
-          ConfigurationTarget.Global
-        );
+    if (apiKey) {
+      await workspace.getConfiguration().update("notebook-chatcompletion.openaiApiKey", apiKey, ConfigurationTarget.Global);
 
       await window.showInformationMessage(
         "Please note that the model is set to GPT-4 by default, which you may not be able to access yet. As a result, the API may return an HTTP 404 error. You can change the 'Default Model' setting to another model or use the 'Set Model' command in the menu to set the model for a specific notebook.",
         { modal: true }
       );
     } else {
-      // If the user didn't provide an API key, show an error message and return
-      window.showErrorMessage(
-        "OpenAI API Key is required for Notebook ChatCompletion to work.",
-        { modal: true }
-      );
+      window.showErrorMessage("OpenAI API Key is required for Notebook ChatCompletion to work.", { modal: true });
       return "";
     }
   }
 
-  return openaiApiKey;
+  return apiKey;
 }
 
 type TokenReductionStrategy = QuickPickItem & {
@@ -332,7 +257,7 @@ type TokenReductionStrategy = QuickPickItem & {
 };
 
 async function applyTokenReductionStrategies(
-  messages: ChatCompletionRequestMessage[],
+  msgs: ChatCompletionRequestMessage[],
   tokenOverflowCount: number,
   totalTokenCount: number,
   limit: number,
@@ -342,26 +267,19 @@ async function applyTokenReductionStrategies(
     {
       label: "Remove all Cell Output",
       apply: async () => {
-        return messages.filter(
-          (message) => !message.content.startsWith("Output from previous code:")
-        );
+        return msgs.filter((message) => !message.content.startsWith("Output from previous code:"));
       },
     },
     {
       label: "Remove all VSCode Problems",
       apply: async () => {
-        return messages.filter(
-          (message) =>
-            !message.content.startsWith(
-              "Problems reported by VSCode from previous code:"
-            )
-        );
+        return msgs.filter((message) => !message.content.startsWith("Problems reported by VSCode from previous code:"));
       },
     },
     {
       label: "Remove Spaces",
       apply: async () => {
-        return messages.map((message) => ({
+        return msgs.map((message) => ({
           ...message,
           content: message.content.replace(/ /g, ""),
         }));
@@ -370,7 +288,7 @@ async function applyTokenReductionStrategies(
     {
       label: "Remove Line-breaks",
       apply: async () => {
-        return messages.map((message) => ({
+        return msgs.map((message) => ({
           ...message,
           content: message.content.replace(/\n/g, ""),
         }));
@@ -379,7 +297,7 @@ async function applyTokenReductionStrategies(
     {
       label: "Remove Punctuations",
       apply: async () => {
-        return messages.map((message) => ({
+        return msgs.map((message) => ({
           ...message,
           content: message.content.replace(/[.,;:!?]/g, ""),
         }));
@@ -387,7 +305,6 @@ async function applyTokenReductionStrategies(
     },
   ];
 
-  // Calculate token savings for each strategy
   for (const strategy of strategies) {
     const reducedMessages = await strategy.apply();
     const reducedTokenCount = countTotalTokens(reducedMessages, model);
@@ -396,14 +313,9 @@ async function applyTokenReductionStrategies(
     strategy.description = `${savedTokens} tokens`;
   }
 
-  // remove strategies that wouldn't reduce tokens
-  strategies = strategies.filter((s) =>
-    s.savedTokens ? s.savedTokens > 1 : false
-  );
+  strategies = strategies.filter((s) => (s.savedTokens ? s.savedTokens > 1 : false));
 
-  const maxPossibleSaving = strategies
-    .map((x) => x.savedTokens ?? 0)
-    .reduce((prev, current) => prev + current);
+  const maxPossibleSaving = strategies.map((x) => x.savedTokens ?? 0).reduce((prev, current) => prev + current);
 
   if (maxPossibleSaving < tokenOverflowCount) {
     window.showInformationMessage(
@@ -414,7 +326,6 @@ async function applyTokenReductionStrategies(
     );
   }
 
-  // Show the dialogue with strategies
   const selectedStrategies = await window.showQuickPick(strategies, {
     canPickMany: true,
     title: "Too many tokens",
@@ -425,18 +336,14 @@ async function applyTokenReductionStrategies(
     return null;
   }
 
-  // Apply the selected strategies
-  let reducedMessages = messages;
+  let reducedMessages = msgs;
   for (const strategy of selectedStrategies) {
     reducedMessages = await strategy.apply(reducedMessages);
   }
 
-  // Validate the total reduction
   const reducedTokenCount = countTotalTokens(reducedMessages, model);
   if (reducedTokenCount > limit) {
-    window.showErrorMessage(
-      "The selected strategies do not reduce tokens below the limit."
-    );
+    window.showErrorMessage("The selected strategies do not reduce tokens below the limit.");
     return null;
   }
 
@@ -450,11 +357,8 @@ function countTokens(text: string, model: any): number {
   return tokenCount;
 }
 
-function countTotalTokens(
-  messages: ChatCompletionRequestMessage[],
-  model: string
-): number {
-  return messages.reduce((accumulator, message) => {
+function countTotalTokens(msgs: ChatCompletionRequestMessage[], model: string): number {
+  return msgs.reduce((accumulator, message) => {
     return accumulator + countTokens(message.content, model);
   }, 0);
 }
