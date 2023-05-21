05f8f04763b34341f6d4cd8cad40c61a58af4bff
Alexandre De Riemaecker <riemaecker@gmail.com>
2023-05-21 15:14:21 +0200

    adds helper function for waiting on UI dispatch

--- src/uiProgress.ts
+++ src/uiProgress.ts
@@ -4,3 +4,9 @@ export type UIProgress = Progress<{
   message?: string | undefined;
   increment?: number | undefined;
 }>;
+
+export async function waitForUIDispatch() {
+  // this is a workaround to make sure the async dispatch queue has been processed
+  // so that we the progress window is shown before the current scope blocks the dispatch queue with large work items
+  await new Promise<void>((r) => setTimeout(r, 0));
+}
